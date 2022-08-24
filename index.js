3b68eb03cf5df4276993a5e72a3becccf7505553
Julien Jamet <julien.jamet.34@gmail.com>
2022-08-24 17:39:09 +0200

    ready to use

--- index.js
+++ index.js
@@ -2,42 +2,175 @@
 const colorOne = document.getElementById("colorOne");
 const colorTwo = document.getElementById("colorTwo");
 const colorThree = document.getElementById("colorThree");
+const colorFour = document.getElementById("colorFour");
+const colorFive = document.getElementById("colorFive");
+const colorSix = document.getElementById("colorSix");
+const colorSeven = document.getElementById("colorSeven");
+const colorEight = document.getElementById("colorEight");
+const colorNine = document.getElementById("colorNine");
+const colorTen = document.getElementById("colorTen");
 /*---------------------------------------------------------*/
 
 
-/*Définition des variables canvas------------------------*/
+/*Définition des variables mouvements------------------------*/
+const translationX = document.getElementById("translationX");
+const translationY = document.getElementById("translationY");
+const rotation = document.getElementById("rotation");
+const timing = document.getElementById("timing");
+/*---------------------------------------------------------*/
+
+
+/*Définition des variables canvas--------------------------*/
 const monCanvas = document.getElementById('dessin');
 const ctx = monCanvas.getContext('2d');
 /*---------------------------------------------------------*/
 
 
-/*Initialisation des variables couleurs--------------------*/
-let colorOneChoice = "red";
-let colorTwoChoice = "yellow";
-let colorThreeChoice = "blue";
+/*Initialisation des variables canvas----------------------*/
+let lingrad;
+let panel = getPanel();
 /*---------------------------------------------------------*/
 
 
-/*Initialisation des variables canvas--------------------*/
-let lingrad;
-let panel = {};
+/*Initialisation des variables couleurs--------------------*/
+let colorOneChoice = panel.colorOne;
+let colorTwoChoice = panel.colorTwo;
+let colorThreeChoice = panel.colorThree;
+let colorFourChoice = panel.colorFour;
+let colorFiveChoice = panel.colorFive;
+let colorSixChoice = panel.colorSix;
+let colorSevenChoice = panel.colorSeven;
+let colorEightChoice = panel.colorEight;
+let colorNineChoice = panel.colorNine;
+let colorTenChoice = panel.colorTen;
+/*---------------------------------------------------------*/
+
+
+/*Initialisation des variables mouvements--------------------*/
+let translationXChoice = panel.translationX;
+let translationYChoice = panel.translationY;
+let rotationChoice = panel.rotation;
+let timingChoice = panel.timing;
 /*---------------------------------------------------------*/
 
 
 /*Modification des variables couleurs----------------------*/
 colorOne.addEventListener("change", function(e) {
-    colorOneChoice = e.target.value;
-    console.log(colorOneChoice);
+    if (colorOneChoice == undefined) {
+        colorOneChoice = panel.colorOne;
+    }
+    else {
+        colorOneChoice = e.target.value;
+    }
 });
 colorTwo.addEventListener("change", function(e) {
-    colorTwoChoice = e.target.value;
-    console.log(colorTwoChoice);
+    if (colorTwoChoice == undefined) {
+        colorTwoChoice = panel.colorTwo;
+    }
+    else {
+        colorTwoChoice = e.target.value;
+    }
 });
 colorThree.addEventListener("change", function(e) {
-    colorThreeChoice = e.target.value;
-    console.log(colorThreeChoice);
+    if (colorThreeChoice == undefined) {
+        colorThreeChoice = panel.colorThree;
+    }
+    else {
+        colorThreeChoice = e.target.value;
+    }
+});
+colorFour.addEventListener("change", function(e) {
+    if (colorFourChoice == undefined) {
+        colorFourChoice = panel.colorFour;
+    }
+    else {
+        colorFourChoice = e.target.value;
+    }
+});
+colorFive.addEventListener("change", function(e) {
+    if (colorFiveChoice == undefined) {
+        colorFiveChoice = panel.colorFive;
+    }
+    else {
+        colorFiveChoice = e.target.value;
+    }
+});
+colorSix.addEventListener("change", function(e) {
+    if (colorSixChoice == undefined) {
+        colorSixChoice = panel.colorSix;
+    }
+    else {
+        colorSixChoice = e.target.value;
+    }
+});
+colorSeven.addEventListener("change", function(e) {
+    if (colorSevenChoice == undefined) {
+        colorSevenChoice = panel.colorSeven;
+    }
+    else {
+        colorSevenChoice = e.target.value;
+    }
+});
+colorEight.addEventListener("change", function(e) {
+    if (colorEightChoice == undefined) {
+        colorEightChoice = panel.colorEight;
+    }
+    else {
+        colorEightChoice = e.target.value;
+    }
+});
+colorNine.addEventListener("change", function(e) {
+    if (colorNineChoice == undefined) {
+        colorNineChoice = panel.colorNine;
+    }
+    else {
+        colorNineChoice = e.target.value;
+    }
+});
+colorTen.addEventListener("change", function(e) {
+    if (colorTenChoice == undefined) {
+        colorTenChoice = panel.colorTen;
+    }
+    else {
+        colorTenChoice = e.target.value;
+    }
 });
 /*---------------------------------------------------------*/
+
+
+/*Modification des variables mouvements----------------------*/
+translationX.addEventListener("change", function(e) {
+    if (translationXChoice == undefined) {
+        translationXChoice = panel.translationX;
+    }
+    else {
+        translationXChoice = e.target.value;
+    }
+});
+translationY.addEventListener("change", function(e) {
+    if (translationYChoice == undefined) {
+        translationYChoice = panel.translationY;
+    }
+    else {
+        translationYChoice = e.target.value;
+    }
+});
+rotation.addEventListener("change", function(e) {
+    if (rotationChoice == undefined) {
+        rotationChoice = panel.rotation;
+    }
+    else {
+        rotationChoice = e.target.value;
+    }
+});
+timing.addEventListener("change", function(e) {
+    if (timingChoice == undefined) {
+        timingChoice = panel.timing;
+    }
+    else {
+        timingChoice = e.target.value;
+    }
+});
 
 
 /*Sauvegarde et récupération du localStorage---------------*/  
@@ -63,54 +196,67 @@ submit.addEventListener("click", function(e) {
     let panel = {
         colorOne: colorOneChoice,
         colorTwo: colorTwoChoice,
-        colorThree: colorThreeChoice
+        colorThree: colorThreeChoice,
+        colorFour: colorFourChoice,
+        colorFive: colorFiveChoice,
+        colorSix: colorSixChoice,
+        colorSeven: colorSevenChoice,
+        colorEight: colorEightChoice,
+        colorNine: colorNineChoice,
+        colorTen: colorTenChoice,
+        translationX: translationXChoice,
+        translationY: translationYChoice,
+        rotation: rotationChoice,
+        timing: timingChoice
     };
-    savePanel(panel);
-    window.location.reload();
+        savePanel(panel);
+        window.location.reload();
 });
 /*---------------------------------------------------------*/
 
 
-/*Ajout du panel dans le localStorage-----------------------*/
+/*Récupération du panel et application dans le DOM-----------------------*/
 window.addEventListener("DOMContentLoaded", function() {
     let panel = getPanel();
-    console.log(panel.colorOne);
+
+    colorOne.setAttribute("value", `${panel.colorOne}`);
+    colorTwo.setAttribute("value", `${panel.colorTwo}`);
+    colorThree.setAttribute("value", `${panel.colorThree}`);
+    colorFour.setAttribute("value", `${panel.colorFour}`);
+    colorFive.setAttribute("value", `${panel.colorFive}`);
+    colorSix.setAttribute("value", `${panel.colorSix}`);
+    colorSeven.setAttribute("value", `${panel.colorSeven}`);
+    colorEight.setAttribute("value", `${panel.colorEight}`);
+    colorNine.setAttribute("value", `${panel.colorNine}`);
+    colorTen.setAttribute("value", `${panel.colorTen}`);
+    translationX.setAttribute("value", `${panel.translationX}`);
+    translationY.setAttribute("value", `${panel.translationY}`);
+    timing.setAttribute("value", `${panel.timing}`);
 
     lingrad = ctx.createLinearGradient(0,0,200,0);
 
     lingrad.addColorStop(0, `${panel.colorOne}`);
-    lingrad.addColorStop(0.05, `${panel.colorTwo}`);
-    lingrad.addColorStop(0.1, `${panel.colorThree}`);
+    lingrad.addColorStop(0.1, `${panel.colorTwo}`);
+    lingrad.addColorStop(0.2, `${panel.colorThree}`);
+    lingrad.addColorStop(0.3, `${panel.colorFour}`);
+    lingrad.addColorStop(0.4, `${panel.colorFive}`);
+    lingrad.addColorStop(0.5, `${panel.colorSix}`);
+    lingrad.addColorStop(0.6, `${panel.colorSeven}`);
+    lingrad.addColorStop(0.7, `${panel.colorEight}`);
+    lingrad.addColorStop(0.8, `${panel.colorNine}`);
+    lingrad.addColorStop(0.9, `${panel.colorTen}`);
 
     ctx.fillStyle = lingrad;
 
     ctx.translate(monCanvas.width/1.5, monCanvas.height/2.5);
 
     let i = 0;
-    const inter = setInterval(rotation, 5);
+    const inter = setInterval(rotation, `${panel.timing}`);
 
     function rotation() {
-        ctx.translate(14,14.7);
-        ctx.rotate(5);
+        ctx.translate(`${panel.translationX}`, `${panel.translationY}`);
+        ctx.rotate(`${panel.rotation}`);
         ctx.fillRect(0,0,200,10);
         i++;
     };
 }); 
-    /*lingrad.addColorStop(0.15, "green");
-    lingrad.addColorStop(0.2, "black");
-    lingrad.addColorStop(0.25, "green");
-    lingrad.addColorStop(0.3, "silver");
-    lingrad.addColorStop(0.35, "green");
-    lingrad.addColorStop(0.4, "black");
-    lingrad.addColorStop(0.45, "orange");
-    lingrad.addColorStop(0.5, "silver");
-    lingrad.addColorStop(0.55, "green");
-    lingrad.addColorStop(0.6, "black");
-    lingrad.addColorStop(0.65, "yellowgreen");
-    lingrad.addColorStop(0.7, "black");
-    lingrad.addColorStop(0.75, "silver");
-    lingrad.addColorStop(0.8, "orange");
-    lingrad.addColorStop(0.85, "black");
-    lingrad.addColorStop(0.9, "yellowgreen");
-    lingrad.addColorStop(0.95, "green");
-    lingrad.addColorStop(1, "silver");*/
